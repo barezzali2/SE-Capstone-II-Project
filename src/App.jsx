0bc040425c727a36f86f688560e3d4f048e0e949
barezzali2 <barezrealy22@gmail.com>
2025-02-23 23:13:29 +0300

    Stage 1 - Using react router and calling the pages here using the tools and elements related to router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Search from "./pages/Search";
+import Scanner from "./pages/Scanner";
+
 
 function App() {
 
   return (
-      <div>
-
-      </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Navigate to="/home" replace />}/>
+              <Route path="/home" element={<Home />}/>
+              <Route path="productlist" element={<ProductList />}/>
+              <Route path="search" element={<Search />}/>
+              <Route path="scanner" element={<Scanner />}/>
+          </Routes>
+      </BrowserRouter>
   )
 }
 
