65632e0100d2778277895e71e8b90cafe78570f4
barezzali2 <barezrealy22@gmail.com>
2025-02-24 22:19:15 +0300

    Day 2 - Having Shopping card page

---
+++ src/pages/ShoppingCard.jsx
@@ -0,0 +1,11 @@
+import styles from "./ShoppingCard.module.css";
+
+function ShoppingCard() {
+  return (
+    <div className={styles.shopping}>
+        ShoppingCard
+    </div>
+  )
+}
+
+export default ShoppingCard;
