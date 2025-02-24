501f19e9db7ad4e1fca00e6298908d3aa531bc63
barezzali2 <barezrealy22@gmail.com>
2025-02-24 15:50:35 +0300

    Day 2 - Styling the footer better

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,9 @@
 import styles from "./Footer.module.css";
+import { BsFillTelephoneInboundFill } from "react-icons/bs";
+import { BiLogoGmail } from "react-icons/bi";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
 
 function Footer() {
   return (
@@ -7,18 +12,18 @@ function Footer() {
 
         <div className={styles.info}>
           <a href="tel:+9647739737842">
-            <img src="/images/telephone.png" alt="tel" /><h4>Call: +964 123 4567</h4>
+          <BsFillTelephoneInboundFill className={styles.tel} /><h4>Call: +964 123 4567</h4>
           </a>
 
           <a href="mailto:barezz.ali22@gmail.com">
-            <img src="/images/email.png" alt="" /><h4>MyMall@gmail.com</h4>
+          <BiLogoGmail className={styles.email} /><h4>MyMall@gmail.com</h4>
           </a>
         </div>
 
         <div className={styles.socials}>
-            <a href="#" target="_blank"><img src="/images/instagram.png" alt="instagram" /><h5>MyMall</h5></a>
-            <a href="#" target="_blank"><img src="/images/facebook.png" alt="facebook" /><h5>MyMall</h5></a>
-            <a href="#" target="_blank"><img src="/images/tiktok.png" alt="tiktok" /><h5>MyMall</h5></a>
+            <a href="#" target="_blank"><FaInstagram className={styles.social}/><h5>MyMall</h5></a>
+            <a href="#" target="_blank"><FaFacebookF className={styles.social}/><h5>MyMall</h5></a>
+            <a href="#" target="_blank"><FaTiktok className={styles.social}/><h5>MyMall</h5></a>
         </div>
         <p>© 2025 RetailXplore. All rights reserved.</p>
     </footer>
