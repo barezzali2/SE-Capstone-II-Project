4e2481025d633cdbd5a555f251691df7bfcd80fb
barezzali2 <barezrealy22@gmail.com>
2025-02-24 22:15:57 +0300

    Day 2 - Adding link to the map image in home

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
   return (
     <div className={styles.map}>
-        <div> 
+        <Link to="/map"> 
             <img src="/images/dummymap.png" alt="map" className={styles.map3D}/>
             <img src="/images/touch.png" alt="touch" className={styles.touch}/>
-        </div>
+        </Link>
     </div>
   )
 }
