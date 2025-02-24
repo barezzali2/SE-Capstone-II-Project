c6ab5786641480487d8377e490034cb07ee402a7
barezzali2 <barezrealy22@gmail.com>
2025-02-24 11:19:52 +0300

    Day 2 - Modifying home page to include the new components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
-import Navbar from "../components/Navbar";
 import styles from "./Home.module.css";
+import Map from "../components/Map";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
     <div className={styles.home}>
         <Navbar />
+        <Map />
+        <Footer />
     </div>
   )
 }
