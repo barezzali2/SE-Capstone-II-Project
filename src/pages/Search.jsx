bf7c158e1af6ad8a91446e00947234c8c32c10a8
barezzali2 <barezrealy22@gmail.com>
2025-02-26 10:23:04 +0300

    Day 4 - Adding footer for Search Page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,14 @@
-import SearchBar from "../components/SearchBar";
 import styles from "./Search.module.css";
+import SearchBar from "../components/SearchBar";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer"
+
 function Search() {
   return (
     <div className={styles.search}>
       <Navbar />
       <SearchBar />
+      <Footer />
     </div>
   );
 }
