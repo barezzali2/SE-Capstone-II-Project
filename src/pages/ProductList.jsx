023839d0b724f5c258c53945d56310084fb09c0c
Zhiyar_Qaradaghi <zhiyar.burhan2003@gmail.com>
2025-02-26 10:33:15 +0300

    Day 4 - Added product list (filter component - product component - list component)

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../components/Navbar";
 import styles from "./ProductList.module.css";
+import List from "../components/List";
 
 function ProductList() {
   return (
     <div className={styles.productList}>
-        <Navbar />
+      <Navbar />
+      <List />
     </div>
-  )
+  );
 }
 
 export default ProductList;
