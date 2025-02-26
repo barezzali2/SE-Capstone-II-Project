d32670f04144b781573d85b5aaeff6eb07ec77f8
barezzali2 <barezrealy22@gmail.com>
2025-02-27 00:39:27 +0300

    Day 4 - Adding a greeting banner, and calling RecentList

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,18 @@ import styles from "./Home.module.css";
 import Map from "../components/Map";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import RecentList from "../components/RecentList";
 
 function Home() {
   return (
     <div className={styles.home}>
         <Navbar />
+        <div className={styles.greetingBanner}>
+          <h2>Hello! This is Retail<span>Xplore</span>🛍️</h2>
+          <h4>Explore and make your shopping easier with us!</h4>
+        </div>
         <Map />
+        <RecentList />
         <Footer />
     </div>
   )
