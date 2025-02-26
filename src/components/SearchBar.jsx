223e7c51cc8b98f925e45a26b30f71acde435f22
barezzali2 <barezrealy22@gmail.com>
2025-02-26 10:22:01 +0300

    Day 4 - Adding modifying the nameing conventions for this component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,17 +9,17 @@ function SearchBar() {
   };
 
   return (
-    <div className={styles["search-container"]}>
-      <div className={styles["search-wrapper"]}>
+    <div className={styles.searchContainer}>
+      <div className={styles.searchWrapper}>
         <input
           type="search"
           placeholder="Search..."
-          className={styles["search-input"]}
+          className={styles.searchInput}
           value={searchQuery}
           onChange={handleSearch}
         />
-        <button className={styles["search-button"]}> 
-          <svg className={styles["search-icon"]} viewBox="0 0 24 24">
+        <button className={styles.searchButton}> 
+          <svg className={styles.searchIcon} viewBox="0 0 24 24">
             <path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 1 0-.7.7l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0A4.5 4.5 0 1 1 14 9.5 4.5 4.5 0 0 1 9.5 14z" />
           </svg>
         </button>
