645bd76136c99634b215285852215b41f974aa9b
barezzali2 <barezrealy22@gmail.com>
2025-02-27 21:52:51 +0300

    Day 5 - modifying the Product list based on the context we have added

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,69 +2,13 @@ import { useState } from "react";
 import styles from "./List.module.css";
 import Filter from "./Filter";
 import Product from "./Product";
+import { useProduct } from "../contexts/ProductContext";
 
 function List() {
-  // dummmy products to test
-  const [products] = useState([
-    {
-      id: 1,
-      name: "Classic White Sneakers",
-      image: "https://picsum.photos/400/400?random=1",
-      price: "$99.99",
-      category: "Footwear",
-    },
-    {
-      id: 2,
-      name: "Leather Backpack",
-      image: "https://picsum.photos/400/400?random=2",
-      price: "$129.99",
-      category: "Accessories",
-    },
-    {
-      id: 3,
-      name: "Denim Jacket",
-      image: "https://picsum.photos/400/400?random=3",
-      price: "$89.99",
-      category: "Outerwear",
-    },
-    {
-      id: 4,
-      name: "Cotton T-Shirt",
-      image: "https://picsum.photos/400/400?random=4",
-      price: "$29.99",
-      category: "Tops",
-    },
-    {
-      id: 5,
-      name: "Slim Fit Jeans",
-      image: "https://picsum.photos/400/400?random=5",
-      price: "$79.99",
-      category: "Bottoms",
-    },
-    {
-      id: 6,
-      name: "Sports Watch",
-      image: "https://picsum.photos/400/400?random=6",
-      price: "$199.99",
-      category: "Accessories",
-    },
-    {
-      id: 7,
-      name: "Running Shoes",
-      image: "https://picsum.photos/400/400?random=7",
-      price: "$119.99",
-      category: "Footwear",
-    },
-    {
-      id: 8,
-      name: "Wool Sweater",
-      image: "https://picsum.photos/400/400?random=8",
-      price: "$69.99",
-      category: "Tops",
-    },
-  ]);
-  
+
+  const {products} = useProduct();
   const [filteredProducts, setFilteredProducts] = useState(products);
+
 
   const handleSort = (sortType) => {
     let sorted = [...filteredProducts];
@@ -104,6 +48,7 @@ function List() {
 
     setFilteredProducts(filtered);
   };
+
 
   return (
     <div className={styles.listContainer}>
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,12 +2,16 @@ import styles from "./ProductList.module.css";
 import Navbar from "../components/Navbar";
 import List from "../components/List";
 import Footer from "../components/Footer";
+import { ProductProvider } from "../contexts/ProductContext";
+
 
 function ProductList() {
   return (
     <div className={styles.productList}>
       <Navbar />
-      <List />
+      <ProductProvider>
+        <List />
+      </ProductProvider>
       <Footer />
     </div>
   );
