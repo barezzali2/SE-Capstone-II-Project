336f14d792bc78cad50e8ac3542c2eeb73fc04a3
barezzali2 <barezrealy22@gmail.com>
2025-03-05 20:58:32 +0300

    Day 11 - contents for the shopping cart page

---
+++ src/components/CartWishlist.jsx
@@ -0,0 +1,30 @@
+import styles from "./Cartwishlist.module.css";
+import { useProduct } from "../contexts/ProductContext";
+import Cart from "./Cart";
+
+function CardWishlist() {
+    const { products } = useProduct();
+    const addedProducts = products.slice(0, 2);
+
+    return (
+        <>
+            <div className={styles.wishlist}>
+                <h3>Shopping Cart Wishlist</h3>
+                <p>X items in the Cart</p>
+
+                <div className={styles.carts}>
+                    {addedProducts.map((product) => (
+                        <Cart key={product.id} product={product} />
+                    ))}
+                </div>
+            </div>
+
+
+            <div className={styles.proceed}>
+
+            </div>
+        </>
+    )
+}
+
+export default CardWishlist;
