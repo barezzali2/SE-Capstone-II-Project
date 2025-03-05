bbbffa9ccf6afd5aeabc22e42ea1bf480abb9c39
barezzali2 <barezrealy22@gmail.com>
2025-03-05 20:59:22 +0300

    Day 11 - Some modification

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,7 +1,7 @@
 import styles from "./ShoppingCart.module.css";
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer";
-import CardWishlist from "../components/CardWishlist";
+import CartWishlist from "../components/CartWishlist";
 import { ProductProvider } from "../contexts/ProductContext";
 
 function ShoppingCart() {
@@ -9,7 +9,7 @@ function ShoppingCart() {
     <div className={styles.shopping}>
         <Navbar />
         <ProductProvider>
-            <CardWishlist />
+            <CartWishlist />
         </ProductProvider>
         <Footer />
     </div>
