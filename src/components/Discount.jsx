ea5bf1af886602f188170e11fb1104aee8ebebb1
barezzali2 <barezrealy22@gmail.com>
2025-03-07 14:28:42 +0300

    Day 13 - All is well

--- src/components/Discount.jsx
+++ src/components/Discount.jsx
@@ -12,11 +12,11 @@ function Discount() {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 440,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 2300,
     responsive: [
       {
         breakpoint: 1024,
