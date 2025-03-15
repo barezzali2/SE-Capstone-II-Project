eb4482a684b6ec98f3768d3e6d241abaccb47da8
Zhiyar_Qaradaghi <zhiyar.burhan2003@gmail.com>
2025-03-16 02:32:12 +0300

    Day 21 - Added Product Counter to Cart Icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,13 @@ import Logo from "./Logo";
 import styles from "./Navbar.module.css";
 import { memo, useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
+import { useCart } from "../contexts/CartContext";
 
 // Memo
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { cart } = useCart();
+  const totalItems = cart?.totalItems || 0;
 
   function handleMenuOpen() {
     setMenuOpen((prev) => !prev);
@@ -14,75 +17,93 @@ function Navbar() {
 
   return (
     <div className={styles.navbar}>
-        <Logo />
+      <Logo />
 
+      <ul className={styles.ulDefault}>
+        <li>
+          <NavLink
+            to="/home"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            HOME
+          </NavLink>
+        </li>
 
-        <ul className={styles.ulDefault}>
-          <li>
-            <NavLink to="/home" className={({isActive}) => (isActive ? styles.active : "")}>
-              HOME
-            </NavLink>
-          </li>
+        <li>
+          <NavLink
+            to="/productlist"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            PRODUCT LIST
+          </NavLink>
+        </li>
 
-          <li>
-            <NavLink to="/productlist" className={({isActive}) => (isActive ? styles.active : "")}>
-              PRODUCT LIST
-            </NavLink>
-          </li>
+        <li>
+          <NavLink
+            to="/search"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            SEARCH
+          </NavLink>
+        </li>
 
-          <li>
-            <NavLink to="/search" className={({isActive}) => (isActive ? styles.active : "")}>
-              SEARCH
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/scanner" className={({isActive}) => (isActive ? styles.active : "")}> 
-              SCANNER
-            </NavLink>
-          </li>
+        <li>
+          <NavLink
+            to="/scanner"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            SCANNER
+          </NavLink>
+        </li>
       </ul>
 
-        
-        <div className={styles.shoppingBtn}>
-          <Link to="/shopping"><FaShoppingCart className={styles.shoppingCard}/></Link>
-        </div>
+      <div className={styles.shoppingBtn}>
+        <Link to="/shopping">
+          <div className={styles.cartIconContainer}>
+            <FaShoppingCart className={styles.shoppingCard} />
+            {totalItems > 0 && (
+              <span className={styles.cartBadge}>{totalItems}</span>
+            )}
+          </div>
+        </Link>
+      </div>
 
-
-        <div className={`${styles.hamburger} ${menuOpen ? styles.open : ""}`} onClick={handleMenuOpen}>
+      <div
+        className={`${styles.hamburger} ${menuOpen ? styles.open : ""}`}
+        onClick={handleMenuOpen}
+      >
         <span></span>
         <span></span>
         <span></span>
       </div>
 
-
       <ul className={`${styles.ulMob} ${menuOpen ? styles.open : ""}`}>
-          <li>
-            <NavLink to="/home" onClick={handleMenuOpen}>
-              HOME
-            </NavLink>
-          </li>
+        <li>
+          <NavLink to="/home" onClick={handleMenuOpen}>
+            HOME
+          </NavLink>
+        </li>
 
-          <li>
-            <NavLink to="/productlist" onClick={handleMenuOpen}>
-              PRODUCT LIST
-            </NavLink>
-          </li>
+        <li>
+          <NavLink to="/productlist" onClick={handleMenuOpen}>
+            PRODUCT LIST
+          </NavLink>
+        </li>
 
-          <li>
-            <NavLink to="/search" onClick={handleMenuOpen}>
-              SEARCH
-            </NavLink>
-          </li>
+        <li>
+          <NavLink to="/search" onClick={handleMenuOpen}>
+            SEARCH
+          </NavLink>
+        </li>
 
-          <li>
-            <NavLink to="/scanner" onClick={handleMenuOpen}> 
-              SCANNER
-            </NavLink>
-          </li>
+        <li>
+          <NavLink to="/scanner" onClick={handleMenuOpen}>
+            SCANNER
+          </NavLink>
+        </li>
       </ul>
     </div>
-  )
-};
+  );
+}
 
 export default memo(Navbar);
