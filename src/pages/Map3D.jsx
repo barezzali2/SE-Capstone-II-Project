b458b7ad02d4630ff23516faeccfb6c8c437e1d2
barezzali2 <barezrealy22@gmail.com>
2025-03-17 14:43:48 +0300

    Day 23 - Keeping track with the changes

--- src/pages/Map3D.jsx
+++ src/pages/Map3D.jsx
@@ -1,11 +1,13 @@
 import styles from "./Map3D.module.css";
 import Navbar from "../components/Navbar"
+import MapView from "../components/MapView";
 import Footer from "../components/Footer"
 
 function Map3D() {
   return (
     <div className={styles.map}>
         <Navbar />
+        <MapView />
         <Footer />
     </div>
   )
