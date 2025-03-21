727f245e8b1b9c036af51f8e3241069365b8290f
barezzali2 <barezrealy22@gmail.com>
2025-03-22 01:29:54 +0300

    Day 26 - Added review page

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,9 +1,17 @@
-import StarRating from '../components/StarRating';
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import styles from "./Review.module.css";
+import FeedbackReview from "../components/FeedbackReview";
+import { useLocation } from "react-router-dom";
 
 function Review() {
+  const location = useLocation();
+  const product = location.state?.product;
   return (
-    <div>
-      Hello from Review
+    <div className={styles.review}>
+      <Navbar />
+      <FeedbackReview product={product}/>
+      <Footer />
     </div>
   )
 }
