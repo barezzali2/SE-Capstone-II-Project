f3500103e0d871e3e6e304971dc86f136d2bf166
Zhiyar_Qaradaghi <zhiyar.burhan2003@gmail.com>
2025-03-28 15:49:01 +0300

    Update app from main

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,41 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import { ProductProvider } from "./contexts/ProductContext";
 import { CartProvider } from "./contexts/CartContext";
-import { useState, useEffect } from "react";
-
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Search from "./pages/Search";
-import Scanner from "./pages/Scanner";
-import ShoppingCart from "./pages/ShoppingCart";
-import Map3D from "./pages/Map3D";
 import Loading from "./components/Loading";
 
+// import Home from "./pages/Home";
+// import ProductList from "./pages/ProductList";
+// import Search from "./pages/Search";
+// import Scanner from "./pages/Scanner";
+// import ShoppingCart from "./pages/ShoppingCart";
+// import Map3D from "./pages/Map3D";
+
+const Home = lazy(() => import("./pages/Home"));
+const ProductList = lazy(() => import("./pages/ProductList"));
+const Search = lazy(() => import("./pages/Search"));
+const Scanner = lazy(() => import("./pages/Scanner"));
+const ShoppingCart = lazy(() => import("./pages/ShoppingCart"));
+const Map3D = lazy(() => import("./pages/Map3D"));
+const Review = lazy(() => import("./pages/Review"));
+
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Check if it's the first load in this session
-    const isFirstLoad = sessionStorage.getItem("isFirstLoad") === null;
-
-    if (isFirstLoad) {
-      // Simulate a 3-4 second loading delay
-      const timer = setTimeout(() => {
-        setIsLoading(false);
-        sessionStorage.setItem("isFirstLoad", "false"); // Set flag after first load
-      }, 3500); // 4 seconds delay
-
-      return () => clearTimeout(timer); // Cleanup timer on unmount
-    } else {
-      // If it's not the first load, skip the delay
-      setIsLoading(false);
-    }
-  }, []);
-
   return (
     <ProductProvider>
       <CartProvider>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" replace />} />
-            <Route path="/home" element={isLoading ? <Loading /> : <Home />} />
-            <Route path="home" element={<Home />} />
-            <Route path="productlist" element={<ProductList />} />
-            <Route path="search" element={<Search />} />
-            <Route path="scanner" element={<Scanner />} />
-            <Route path="shopping" element={<ShoppingCart />} />
-            <Route path="map" element={<Map3D />} />
-          </Routes>
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<Navigate to="/home" replace />} />
+              <Route path="home" element={<Home />} />
+              <Route path="productlist" element={<ProductList />} />
+              <Route path="search" element={<Search />} />
+              <Route path="scanner" element={<Scanner />} />
+              <Route path="shopping" element={<ShoppingCart />} />
+              <Route path="map" element={<Map3D />} />
+              <Route path="review" element={<Review />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CartProvider>
     </ProductProvider>
