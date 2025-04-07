bd294a892512d32c9c2374fbe632cb720ed86cc2
barezzali2 <barezrealy22@gmail.com>
2025-04-07 20:43:35 +0300

    Day 41 - Some modification for the navbar and adding some functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,12 +9,26 @@ function Home() {
   return (
     <div className={styles.home}>
       <Navbar />
-      <div className={styles.greetingBanner}>
-        <h2>
-          Hello! This is Retail<span>Xplore</span>🛍️
-        </h2>
-        <h4>Explore and make your shopping easier with us!</h4>
+      <div className={styles.greetingBannerContainer}>
+          <div className={styles.leftQuote}>
+              {/* <h1>Save Time.<br />Shop Smart.<br />Stress Less.</h1> */}
+              <h1>
+                <span className={styles.line}><span className={styles.save}>Save</span> Time.</span>
+                <span className={styles.line}>Shop <span className={styles.smart}>Smart</span>.</span>
+                <span className={styles.line}><span className={styles.stress}>Stress</span> Less.</span>
+              </h1>
+          </div>
+
+        <div className={styles.greetingBanner}>
+          <h2>
+            Hello! This is Retail<span>Xplore</span>🛍️
+          </h2>
+          <h4>Explore and make your shopping easier with us!</h4>
+          
+        </div>
       </div>
+      
+      
       <MapIndicator />
       <Discount />
       <FeaturedList />  
