b3a29f77dc3dcc7489178a002e6c177d48bf809a
barezzali2 <barezrealy22@gmail.com>
2025-04-14 15:19:43 +0300

    None

--- src/components/admin/FeaturedManagement.jsx
+++ src/components/admin/FeaturedManagement.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import { useAuth } from '../../contexts/AuthContext';
 import { useProduct } from '../../contexts/ProductContext';
