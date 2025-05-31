3da5c07c5a5d3ab72445566e15eca9c985a5609c
Zhiyar_Qaradaghi <zhiyar.burhan2003@gmail.com>
2025-05-31 23:56:16 +0300

    Add MUI Material library and implement loading spinner in Map3D component
    use npm install @mui/material

--- src/pages/Map3D.jsx
+++ src/pages/Map3D.jsx
@@ -1,8 +1,28 @@
 import styles from "./Map3D.module.css";
 import MapView from "../components/map/MapView";
 import Footer from "../components/Footer";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useMarker } from "../contexts/MarkerContext";
 
 function Map3D() {
+  const { loading } = useMarker();
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <CircularProgress size={60} thickness={5} />
+      </div>
+    );
+  }
+
   return (
     <div className={styles.map}>
       <MapView />
